86a12038100f0d623e1e20ec77c1a8563f6999ed
Maurina <Maurina.Davis@gmail.com>
2018-10-30 11:02:13 -0600

    learned typewriter effect in JavaScript

--- main.js
+++ main.js
@@ -1,58 +1,12 @@
-let firstEffect = {};
-firstEffect.opacityIn = [0,1];
-firstEffect.scaleIn = [0.2, 1];
-firstEffect.scaleOut = 3;
-firstEffect.durationIn = 500;
-firstEffect.durationOut = 300;
-firstEffect.delay = 200;
+let i = 0;
+let txt = 'Happy Halloween!';
+let speed = 50;
 
-AnimationEffect.timeline({loop: true})
-.add({
-    targets: '.firstEffect .words-1',
-    opacity: firstEffect.opacityIn,
-    scale: firstEffect.scaleIn,
-    duration: firstEffect.durationIn
-}) .add({
-    targets: '.firstEffect .words-1',
-    opacity: 0,
-    scale: firstEffect.scaleOut,
-    duration: firstEffect.durationOut,
-    easing: "easeInExpo",
-    delay: firstEffect.delay
-}).add({
-    targets: '.firstEffect .words-2',
-    opacity: firstEffect.opacityIn,
-    scale: firstEffect.scaleIn,
-    duration: firstEffect.durationIn
-}) .add({
-    targets: '.firstEffect .words-2',
-    opacity: 0,
-    scale: firstEffect.scaleOut,
-    duration: firstEffect.durationOut,
-    easing: "easeInExpo",
-    delay: firstEffect.delay
-}).add({
-    targets: '.firstEffect .words-3',
-    opacity: firstEffect.opacityIn,
-    scale: firstEffect.scaleIn,
-    duration: firstEffect.durationIn
-}) .add({
-    targets: '.firstEffect .words-3',
-    opacity: 0,
-    scale: firstEffect.scaleOut,
-    duration: firstEffect.durationOut,
-    easing: "easeInExpo",
-    delay: firstEffect.delay
-}).add({
-    targets: '.firstEffect .words-4',
-    opacity: firstEffect.opacityIn,
-    scale: firstEffect.scaleIn,
-    duration: firstEffect.durationIn
-}) .add({
-    targets: '.firstEffect .words-4',
-    opacity: 0,
-    scale: firstEffect.scaleOut,
-    duration: firstEffect.durationOut,
-    easing: "easeInExpo",
-    delay: firstEffect.delay
-});
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("firstEffect") .innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
